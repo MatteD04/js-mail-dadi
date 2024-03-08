e8adbae75476b4417bfd0bbe62e1f8d13c6bac50
MatteD04 <matteo.dantonio04@gmail.com>
2024-03-08 17:50:31 +0100

    feat: add style to dice game

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 //gioco dei dadi
 
-//generare 2 numeri random da 1 a 6
-const computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log(computerNumber);
 
-const userNumber = Math.floor(Math.random() * 6) + 1;
-console.log(userNumber);
+const pullButton = document.querySelector('#btn-pull');
+pullButton.addEventListener('click', function () {
 
-//stabilire qual'è il numero più grande
-let userMessage;
-if(computerNumber === userNumber) {
-    userMessage = 'Pareggio';
+    const numbContainer = document.querySelector('#computer');
+    const numbContainer2 = document.querySelector('#user');
 
-} else if(computerNumber > userNumber) {
-    userMessage = 'Hai perso';
+    //al click del pulsante generare 2 numeri random da 1 a 6
+    const computerNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(computerNumber);
 
-} else {
-    userMessage = 'Hai vinto';
+    const userNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(userNumber);
 
-}
+    //stabilire qual'è il numero più grande
+    let userMessage;
+    if(computerNumber === userNumber) {
+        userMessage = 'Pareggio';
 
-// comunichiamo se ha vinto o ha perso
-alert(userMessage);
+    } else if(computerNumber > userNumber) {
+        userMessage = 'Hai perso';
+
+    } else {
+        userMessage = 'Hai vinto';
+    }
+
+    //scriviam in pagina i numeri
+    const divLi = `<div>${computerNumber}</div>`;
+    numbContainer.innerHTML += divLi;
+
+    const divL = `<div>${userNumber}</div>`;
+    numbContainer2.innerHTML += divL;
+
+    // comunichiamo se l'utente ha vinto o ha perso
+    alert(userMessage);
+
+})
